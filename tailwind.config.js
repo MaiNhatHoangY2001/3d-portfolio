227d2ead60be93786936875a9408aba19fa01a2d
MaiNhatHoangY2001 <hoan44pro@gmail.com>
2024-04-20 11:36:24 +0700

    Add font, color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}",
-  ],
+  content: ['./src/**/*.{html,ts}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'tech-primary': '#1a202c', // Deep shades of blue or green
+        'tech-accent': '#38b2ac', // Electric blue or neon green
+        'tech-dark': '#2d3748', // Dark gray or charcoal
+        // Add more colors as needed
+      },
+    },
   },
   plugins: [],
-}
+};
