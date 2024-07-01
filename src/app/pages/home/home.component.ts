4faaeb06231dc3d5ce3f9ac0ad6bc20581303e09
Hoang Mai Nhat <mnhoang@tma.com.vn>
2024-07-01 16:31:34 +0700

    feat: Add content for portfolio website

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,7 +2,10 @@ import { NgFor } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '@app/components/header/header.component';
 import { LanguageSettingComponent } from '@app/components/language-setting/language-setting.component';
-import { TimeLineComponent } from '@app/components/time-line/time-line.component';
+import { AboutMeComponent } from '@app/components/layout/about-me/about-me.component';
+import { FooterComponent } from '@app/components/layout/footer/footer.component';
+import { LatestWorkComponent } from '@app/components/layout/latest-work/latest-work.component';
+import { SkillComponent } from '@app/components/layout/skill/skill.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -17,9 +20,19 @@ import { ToolbarModule } from 'primeng/toolbar';
     TranslateModule,
     NgFor,
     ButtonModule,
-    TimeLineComponent,
     LanguageSettingComponent,
     ToolbarModule,
+    AboutMeComponent,
+    SkillComponent,
+    LatestWorkComponent,
+    FooterComponent,
   ],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  listIcon = [
+    { alt: 'github', img: '/assets/img/github.png', link: 'https://github.com/MaiNhatHoangY2001' },
+    { alt: 'linked', img: '/assets/img/linked.png', link: '#' },
+    { alt: 'google', img: '/assets/img/google.png', link: '#' },
+    { alt: 'facebook', img: '/assets/img/facebook.png', link: '#' },
+  ];
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'tech-primary': '#1a202c', // Deep shades of blue or green
-        'tech-accent': '#38b2ac', // Electric blue or neon green
-        'tech-dark': '#2d3748', // Dark gray or charcoal
-        // Add more colors as needed
+        primaryColor: 'var(--primary-color)',
+        primaryColorText: 'var(--primary-color-text)',
       },
     },
   },
